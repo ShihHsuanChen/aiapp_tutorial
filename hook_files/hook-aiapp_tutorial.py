e3ef429a72ba2587a77c3027864fa82523d06584
j0018 <j0018@muenai.com>
2024-08-15 17:55:42 +0800

    CHANGE: move pyinstaller --add-data into hook-aiapp_tutorial.py

--- hook_files/hook-aiapp_tutorial.py
+++ hook_files/hook-aiapp_tutorial.py
@@ -2,3 +2,7 @@
 from PyInstaller.utils.hooks import copy_metadata
 
 datas = copy_metadata('aiapp_tutorial') # to solve packaing version missing problem
+# add static file
+datas += [
+    ('./models/mobilenetv4_conv_small.e2400_r224_in1k/', 'models/mobilenetv4_conv_small.e2400_r224_in1k')
+]
