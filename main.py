11f1848133532375c7ee12e462f9f74aea2873af
j0018 <j0018@muenai.com>
2024-08-15 16:06:49 +0800

    CHANGE: implement simple app entrance for pyinstaller

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import os
+import logging
+
 from aiapp_tutorial.cli import cli
 
 
 if __name__ == '__main__':
+    # path setting
+    os.environ['INFER_MODEL_NAME_OR_PATH'] = os.path.join('models', 'mobilenetv4_conv_small.e2400_r224_in1k')
+    # run cli
     cli()
